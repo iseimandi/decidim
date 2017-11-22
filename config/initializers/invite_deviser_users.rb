5011c81f621b100642bc40801a5300d41cd51864
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-11-22 18:20:23 +0100

    Rake task and mailers for importing users

---
+++ config/initializers/invite_deviser_users.rb
@@ -0,0 +1,20 @@
+module Devise
+  module Models
+    module Recoverable
+
+      def send_bootstrap_invitation
+        token = set_reset_password_token
+        send_bootstrap_invitation_instructions_notification(token)
+
+        token
+      end
+
+      protected
+
+      def send_bootstrap_invitation_instructions_notification(token)
+        InitialUserMailer.invitation_email(self, token).deliver_now
+      end
+
+    end
+  end
+end
