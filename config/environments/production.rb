b785182636c87b573275cc2384d12d36b2b2d524
Alberto Miedes <albertomg994@gmail.com>
2018-09-11 11:38:55 +0200

    Deface configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,6 +111,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
   config.action_mailer.default_url_options = { host: ENV.fetch('HOST'), protocol: 'https' }
+
+  config.deface.enabled = ENV['DEFACE_ENABLED'] == 'true'
 end
