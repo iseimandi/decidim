8ea4cdb7526b70ec95577ba74b5aaae79a61c9af
Alberto Miedes <albertomg994@gmail.com>
2019-03-21 12:22:00 +0100

    Increase sidekiq redis timeout for slow non-critical environments

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,10 @@ require "sidekiq/web"
 
 Sidekiq::Logging.logger.level = Rails.logger.level
 
-redis_database = { url: ENV.fetch("REDIS_URL") }
+redis_database = {
+  url: ENV.fetch("REDIS_URL"),
+  network_timeout: Rails.env.staging? ? 10 : 1
+}
 
 Sidekiq.configure_server do |config|
   config.redis = redis_database
